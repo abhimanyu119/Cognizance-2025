ea54dc5757812068c7e70929805db401e5850af3
sahil <deepsahil.online@gmail.com>
2025-03-22 16:03:58 +0530

    client changed

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,7 +5,7 @@ import Landing from "./pages/Landing";
 import Login from "./pages/Login";
 import Signup from "./pages/Signup";
 import Profile from "./pages/Profile";
-import { AuthProvider } from "./contexts/AuthContext";
+import { AuthProvider } from "./contexts/AuthContext"; 
 import { UserProvider } from "./contexts/UserContext";
 import ProtectedRoute from "./components/ProtectedRoute";
 import Loader from "./components/Loader";
